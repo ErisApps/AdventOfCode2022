098d98498662bc2af1e6daa7eddcce5d3c53d0f9
Eris <erisapps@outlook.be>
2022-12-01 18:24:20 +0100

    Initial setup

---
+++ src/AdventOfCode2022/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode2022.Shared;
+
+Console.WriteLine("Hello, World!");
+
+const bool executeAll = true;
+var puzzleInstances = typeof(Program)
+    .Assembly
+    .GetTypes()
+    .Where(x => x.IsAssignableTo(typeof(HappyPuzzleBase)) && x.IsClass && !x.IsAbstract)
+    .OrderBy(x => x.Name)
+    // ReSharper disable once HeuristicUnreachableCode
+    .Take(executeAll ? int.MaxValue : 1)
+    .Select(x => (HappyPuzzleBase) Activator.CreateInstance(x)!)
+    .ToList();
+    
+foreach(var puzzleInstance in puzzleInstances) {
+    Console.WriteLine("Executing puzzle instance {0}", puzzleInstance.GetType().Name);
+
+    Console.WriteLine("Solving part 1...");
+    try
+    {
+        puzzleInstance.SolvePart1();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+    
+    Console.WriteLine("\nSolving part 2...");
+    try
+    {
+        puzzleInstance.SolvePart2();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+    
+    Console.WriteLine();
+}
---
+++ src/AdventOfCode2022/Shared/HappyPuzzleBase.cs
@@ -0,0 +1,11 @@
+namespace AdventOfCode2022.Shared;
+
+public abstract class HappyPuzzleBase
+{
+    protected virtual string AssetName => GetType().Name.ToLowerInvariant() + ".txt";
+    protected string AssetPath() => Path.Combine(Environment.CurrentDirectory, "Assets", AssetName);
+
+    public abstract void SolvePart1();
+
+    public abstract void SolvePart2();
+}
